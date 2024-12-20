4b1febd0dafe760db61ec5328ee3862619969b54
karan2108 <77577426+karan2108@users.noreply.github.com>
2024-12-20 22:10:13 +0530

    Update ToDoList.js

--- ToDoList.js
+++ ToDoList.js
@@ -1,14 +1,68 @@
 let tasks = [];
 
+document.addEventListener("DOMContentLoaded", () => {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks'))
+
+    if(storedTasks) {
+        storedTasks.forEach((task) => tasks.push(task))
+        updateTaskList();
+        updateStats();
+    }
+})
+const saveTasks = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+};
+
 const addTask = () => {
     const taskInput = document.getElementById("taskInput");
     const text = taskInput.value.trim();
 
     if(text) {
         tasks.push({ text : text, completed: false});
-    
+        taskInput.value = "";
         updateTaskList();
+        updateStats();
+        saveTasks();
     }   
+};
+
+const toggleTaskComplete = (index) => {
+    tasks[index].completed = !tasks[index].completed;
+    updateTaskList(); 
+    updateStats();
+    saveTasks();
+};
+
+const deleteTask = (index) => {
+    tasks.splice(index, 1);
+    updateTaskList();
+    updateStats();
+    saveTasks();
+};
+
+const editTask = (index) => {
+    const taskInput = document.getElementById('taskInput')
+    taskInput.value = tasks[index].text;
+
+    tasks.splice(index, 1);
+    updateTaskList();
+    updateStats();
+    saveTasks(); 
+};
+
+const updateStats = () => {
+    const completeTasks = tasks.filter(task => task.completed).length
+    const totalTasks = tasks.length;
+    const progress = (completeTasks / totalTasks) * 100;
+    const progressBar = document.getElementById("progress");
+
+    progressBar.style.width = `${progress}%`;
+
+    document.getElementById("numbers").innerText = `${completeTasks} / ${totalTasks}`
+
+    if(tasks.length && completeTasks === totalTasks) {
+        fireWorks();
+    }
 };
 
 const updateTaskList = () => {
@@ -44,3 +98,34 @@ document.getElementById('newTask').addEventListener('click', function (e) {
     addTask();
     updateTaskList();
 });
+
+
+// taskcompleted 
+const fireWorks = () => {
+    const end = Date.now() + 15 * 1000;
+
+// go Buckeyes!
+const colors = ["#bb0000", "#ffffff"];
+
+(function frame() {
+  confetti({
+    particleCount: 2,
+    angle: 60,
+    spread: 55,
+    origin: { x: 0 },
+    colors: colors,
+  });
+
+  confetti({
+    particleCount: 2,
+    angle: 120,
+    spread: 55,
+    origin: { x: 1 },
+    colors: colors,
+  });
+
+  if (Date.now() < end) {
+    requestAnimationFrame(frame);
+  }
+})();
+};
